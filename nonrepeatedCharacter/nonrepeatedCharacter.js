ff912d490368182219c164fdd835ab6d08226c7f
Arun Suresh <arunvsuresh@gmail.com>
2014-12-17 09:39:13 -0800

    Complete nonrepeated characters.

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,15 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+  // create an empty string
+  // iterate through the input string
+  // concat each char into empty string
+  // if indexOf currentChar is -1, return it
+  var str = '';
+  for (var i = 0; i < string.length; i++) {
+  	if (str.indexOf(string.charAt(i)) === -1) {
+  		str += string.charAt(i);
+  	}
+  }
+  return str[1];
 };
