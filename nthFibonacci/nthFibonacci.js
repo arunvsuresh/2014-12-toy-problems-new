7cd674b8fd2bed38bcca9dee24c4192be69229aa
Arun Suresh <arunvsuresh@gmail.com>
2014-12-19 09:26:33 -0800

    Complete nthFibonacci

--- nthFibonacci/nthFibonacci.js
+++ nthFibonacci/nthFibonacci.js
@@ -21,8 +21,16 @@
  */
 
 var nthFibonacci = function (n) {
-  // TODO: implement me!
+  // (n - 1) + (n - 2)
+
+  if (n === 0) {
+  	return 0;
+  }
+
+  if (n === 1) {
+  	return 1;
+  }
+
+  return nthFibonacci(n - 1) + nthFibonacci(n - 2);
 };
-
-
 
