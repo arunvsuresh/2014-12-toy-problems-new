71b2c24d67b88ffc85bcd408a7d788fbd2a66e54
Arun Suresh <arunvsuresh@gmail.com>
2015-01-02 09:30:06 -0800

    Complete largestProductofThree.

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -10,4 +10,28 @@
 
 var largestProductOfThree = function(array) {
   // TODO: everything
+
+
+  // set up results var
+  var result = 1;
+
+  // if array <= 3 
+  if (array.length <= 3) {
+  	// loop through array and multiply all items
+  	for (var i = 0; i < array.length; i++) {
+  		result = result * array[i];
+  	}
+
+  }
+
+  else {
+	  var sortedArr = array.sort();
+	  for (var i = sortedArr.length - 1; i > sortedArr.length - 4; i--) {
+	  	result = result * sortedArr[i];
+	  }
+  }
+  // sort the array
+
+  return result;
+
 };
