458cb53f6b2a584564664618deb5bc86dda2fb52
Arun Suresh <arunvsuresh@gmail.com>
2015-01-03 14:01:12 -0800

    Mostly complete.

--- htmljQueryColor/main.js
+++ htmljQueryColor/main.js
@@ -6,11 +6,31 @@ $(function(){
   // HINT: the `split` array method is your friend
 
   // TODO: your code here!
+  
+	var para1 = $('p').text();
+	var para1Split = (para1.split(' '));
+	for (var i = 0; i < para1Split.length; i++) {
+		para1Split[i] = '<span>' + para1Split[i] + '</span>';
+	}
+
+	$('p').html(para1Split.join(' '));
+
+	// --------------STEP 2--------------
+	// Next, change spans to random colors, once per second
+
+	// TODO: your code here!
+
+	// var colors = ['red', 'blue', 'green'];
+	// var colorChange = function() {
+
+	// }
 
 
-  // --------------STEP 2--------------
-  // Next, change spans to random colors, once per second
-
-  // TODO: your code here!
+	  // have a setTimeout with an anonymous fn, changing colors at 1000
+	  
+	setInterval(function(){
+		$('span').css({color: "red"});
+	}, 1000);
+   
 
 });
