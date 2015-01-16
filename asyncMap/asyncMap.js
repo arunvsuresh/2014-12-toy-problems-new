781f8e6318941474d6081fd47877b2d3ddd8a0fd
Arun Suresh <arunvsuresh@gmail.com>
2015-01-15 20:27:06 -0800

    aSync attempt.

--- asyncMap/asyncMap.js
+++ asyncMap/asyncMap.js
@@ -38,39 +38,105 @@
 
 var asyncMap = function(tasks, callback){
   /* YOUR CODE HERE */
-  //var funcArgs = Array.prototype.slice.call(arguments[0]);
 
   var results = [];
+  var counter = 0;
   for (var i = 0; i < tasks.length; i++) {
-  	results.push(tasks[i]());
-  }
-
-  return callback(results);
+    (function(i) {
+          
+      tasks[i](function(value){
+  		results[i] = value;
+  		counter++;
+  		if (tasks.length === counter) {
+            callback(results);
+        }
+  	  });
+    })(i);
+  	
+}
 
 };
 
 
-// asyncMap([
-// 	function(){
+
+
+
+/* Implement the function asyncMap:
+ *
+ * asyncMap has two parameters, an array of asynchronous functions (tasks) and a callback.
+ * Each of the tasks takes a separate callback and invokes that callback when complete.
+ *
+ * The callback passed to asyncMap is then performed on the results of the callbacks of the tasks.
+ *
+ * The order of these results should be the same as the order of the tasks.
+ * It is important to note that this is not the order in which the tasks return,
+ * but the order in which they are passed to asyncMap.
+ *
+ * Once all the callbacks of the tasks are returned, asyncMap should invoke the callback
+ * on the results array.
+ *
+ *
+ * Example:
+ *
+ * asyncMap([
+ *  function(cb){
+ *    setTimeout(function(){
+ *      cb('one');
+ *    }, 200);
+ *  },
+ *  function(cb){
+ *    setTimeout(function(){
+ *      cb('two');
+ *    }, 100);
+ *  }
+ * ],
+ *  function(results){
+ *    // the results array will equal ['one','two'] even though
+ *    // the second function had a shorter timeout.
+ *    console.log(results); // ['one', 'two']
+ * });
+ *
+ *
+ */
+
+// var asyncMap = function(tasks, callback){
+//   /* YOUR CODE HERE */
+
+//   var results = [];
+//   for (var i = 0; i < tasks.length; i++) {
+//   	tasks[i](function(value){
+//   		results.push(value);
+//   	});
+//   }
+
+//   callback(results);
+
+//   // tasks.map(function(task, index){
+//   // 	// map the result of calling task to
+
+//   // });
+
+
+
+// };
+
+// debug(asyncMap([
+// 	function(cb){
 // 	setTimeout(function(){
-// 		debug('one');
+// 		cb('one');
 // 	}, 200)
 // },
 
-// function(){
+// function(cb){
 // 	setTimeout(function(){
-// 		debug('one');
-// 	}, 200)
+// 		cb('two');
+// 	}, 100)
 // }
 // ],
 
 // function(results) {
 // 	debug(results);
 // }
-// );
-
-
-
-
+// ));
 
 
