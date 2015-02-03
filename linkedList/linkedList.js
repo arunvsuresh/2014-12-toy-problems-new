f56d0704bf736fe3d0d465730d1012e5a675ad6d
Arun Suresh <arunvsuresh@gmail.com>
2015-02-03 09:58:55 -0800

    First Attempt

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -19,21 +19,60 @@
 
 var LinkedList = function(){
   //fill me in!
+  this.list = {};
+  this.head = null;
+  this.tail = null;
+
 };
 
 //write methods here!
 
-LinkedList.prototype.addToTail = function(
-){
+LinkedList.prototype.addToTail = function(value){
+
+	var newNode = this.makeNode(value);
+
+	// if list.head === null
+	if (this.head === null) {
+		this.head = newNode;
+	} 
+	if (this.tail === null) {
+		this.tail = newNode;
+	} else {
+		this.tail.next = newNode;
+		this.tail = newNode;
+	}
+
 };
 
 LinkedList.prototype.removeHead = function(){
+	var result = this.head.value;
+	this.head.value = this.head.next;
+	this.head = null;
+	return result;
 };
 
-LinkedList.prototype.contains = function(
-){
+LinkedList.prototype.contains = function(target){
+	var currentHead = list.head;
+	while (currentHead !== null) {
+		if (currentHead.value === target) {
+			return true;
+		} else {
+			currentHead = currentHead.next;
+		}
+	}
+	return false;
 };
 
-LinkedList.prototype.makeNode = function(
-){
+LinkedList.prototype.makeNode = function(value){
+	var node = {};
+	node.value = value;
+	node.next = null;
+	return node;
 };
+
+var list = new LinkedList();
+
+
+
+
+
